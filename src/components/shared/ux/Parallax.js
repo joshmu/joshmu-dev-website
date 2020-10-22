7ecc8cf866eb1f976410bea2c503b48602a1bade
Josh Mu <mu@joshmu.com>
2020-10-22 15:46:06 +1000

    :zap: parallax

--- src/components/shared/ux/Parallax.js
+++ src/components/shared/ux/Parallax.js
@@ -4,13 +4,35 @@ import {
   useTransform,
   useViewportScroll,
 } from 'framer-motion'
+import { useLayoutEffect, useRef, useState } from 'react'
 
+const calculateMinHeight = (height, range) => {
+  return height + height * range
+}
 const rand = (min = 0, max = 100) => {
   return Math.floor(Math.random() * (+max - +min)) + +min
 }
 
-const Parallax = ({ rate = 0.5, children, ...props }) => {
+const Parallax = ({ rate = 0.2, children, ...props }) => {
+  const ref = useRef(null)
   const { scrollY } = useViewportScroll()
+  const [offsetTop, setOffsetTop] = useState(0)
+  const [minHeight, setMinHeight] = useState('auto')
+
+  useLayoutEffect(() => {
+    if (!ref.current) return null
+    const onResize = () => {
+      // also add half window height, so true position is centre of screen
+      setOffsetTop(ref.current.offsetTop - globalThis.window.innerHeight / 2)
+      setMinHeight(calculateMinHeight(ref.current.offsetHeight, rate))
+    }
+
+    onResize()
+    window.addEventListener('resize', onResize)
+
+    return () => window.removeEventListener('resize', onResize)
+  }, [ref])
+
   const springConfig = {
     damping: 100,
     stiffness: 500,
@@ -19,16 +41,18 @@ const Parallax = ({ rate = 0.5, children, ...props }) => {
 
   // transforming per px thats why we +1
   const y = useSpring(
-    useTransform(scrollY, [0, 1], [0, rate], {
+    useTransform(scrollY, [offsetTop, offsetTop + 1], [0, rate], {
       clamp: false,
     }),
     springConfig
   )
 
   return (
-    <motion.div style={{ y }} {...props}>
-      {children}
-    </motion.div>
+    <div ref={ref} style={{ minHeight }}>
+      <motion.div style={{ y }} {...props}>
+        {children}
+      </motion.div>
+    </div>
   )
 }
 
