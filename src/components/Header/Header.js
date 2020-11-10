34e9e0a0051d648912425a5504c895cee37b08f6
Josh Mu <mu@joshmu.com>
2020-11-11 09:40:22 +1000

    :hammer: overflow

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,18 +20,24 @@ export default function Header() {
     <div className='fixed z-30 w-full mt-4'>
       <div className='container mx-auto'>
         <div className='flex items-center justify-between'>
+          {/* logo */}
           <div
             // onClick={toggleTheme}
             className='h-full ml-2 text-2xl font-normal uppercase transition-colors duration-200 ease-in-out cursor-pointer hover:text-themeAccent'
           >
             <Compressor text='josh mu' hide='osh ' />
           </div>
+
+          {/* nav menu */}
           <div className='relative flex uppercase'>
+            {/* extra length for menu container */}
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               className='absolute top-0 right-0 w-full h-full transition-all duration-200 -mr-full bg-themeText'
             ></motion.div>
+
+            {/* nav */}
             <motion.ul
               initial='hidden'
               animate='show'
@@ -45,14 +51,7 @@ export default function Header() {
                       handleNavItemClick(item)
                     }}
                     variants={navItemVariant}
-                    style={{
-                      scale: currentView === item ? 1.5 : 1,
-                    }}
-                    className={`${
-                      currentView === item
-                        ? 'active font-semibold'
-                        : 'font-normal'
-                    } uppercase px-2 transition-colors hover:text-themeAccent duration-200 ease-in-out focus:outline-none`}
+                    className='px-2 uppercase transition-colors duration-200 ease-in-out hover:text-themeAccent focus:outline-none'
                   >
                     {item}
                   </motion.button>
