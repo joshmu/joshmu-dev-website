502ca51221a9d743c8010d1d2e6b5ec2737bd735
Josh Mu <mu@joshmu.com>
2020-11-11 14:52:02 +1000

    :zap: favicon

--- src/components/shared/Layout/Layout.js
+++ src/components/shared/Layout/Layout.js
@@ -70,6 +70,28 @@ export default function Layout({ children }) {
           content={metaRef.current.description}
         />
         <meta name='twitter:image' content={metaRef.current.imgUrl} />
+
+        {/* favicon */}
+        {/* https://realfavicongenerator.net/ */}
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
 
       <div className='w-screen overflow-x-hidden font-sans antialiased transition-colors duration-200 ease-in-out debug-screens'>
