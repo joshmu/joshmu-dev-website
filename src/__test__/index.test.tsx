170366f8048573441db24ebba5209ebe3a8b6b46
Josh Mu <mu@joshmu.com>
2020-11-18 12:27:41 +1000

    :zap: update test setup with snapshot

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -1,15 +1,21 @@
+// src/tests/index.test.tsx
+
 import { axe, toHaveNoViolations } from 'jest-axe'
 
+import IndexPage from '@/pages/index'
 import { render } from '@testing-library/react'
-
-import App from '../../pages/index'
 
 expect.extend(toHaveNoViolations)
 
-describe('App', () => {
+describe('Index page', () => {
   test('should have no accessibility violations', async () => {
-    const { container } = render(<App />)
+    const { container } = render(<IndexPage />)
     const results = await axe(container)
     expect(results).toHaveNoViolations()
+  })
+
+  test('matches snapshot', () => {
+    const { asFragment } = render(<IndexPage />)
+    expect(asFragment()).toMatchSnapshot()
   })
 })
