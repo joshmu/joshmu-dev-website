9e1bf0520b20a5548d6145606a2d71d3f0f3cab8
Josh Mu <mu@joshmu.com>
2020-11-30 16:01:28 +1000

    chore: remove redundant tw expiremental config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /**
  * @path /tailwind.config.js
- *
  * @project joshmu-dev-website
  * @file tailwind.config.js
  *
@@ -10,56 +9,48 @@
  * @copyright © 2020 - 2020 MU
  */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
-const plugins = []
-const devOnlyPlugins = [require('tailwindcss-debug-screens')]
+const plugins = [];
+const devOnlyPlugins = [require("tailwindcss-debug-screens")];
 
 module.exports = {
   purge: [
-    './pages/**/*.{ts,tsx, js,jsx}',
-    './src/components/**/*.{ts,tsx,js,jsx}',
+    "./pages/**/*.{ts,tsx, js,jsx}",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Raleway', 'Roboto', ...defaultTheme.fontFamily.sans],
+        sans: ["Raleway", "Roboto", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        themeText: 'var(--text)',
-        themeText2: 'var(--text2)',
-        themeBg: 'var(--background)',
-        themeBg2: 'var(--background2)',
-        themeAccent: 'var(--accent)',
+        themeText: "var(--text)",
+        themeText2: "var(--text2)",
+        themeBg: "var(--background)",
+        themeBg2: "var(--background2)",
+        themeAccent: "var(--accent)",
       },
       opacity: {
-        1: '0.01',
-        2: '0.02',
-        3: '0.03',
-        4: '0.04',
-        5: '0.05',
-        10: '0.10',
-        90: '0.90',
+        1: "0.01",
+        2: "0.02",
+        3: "0.03",
+        4: "0.04",
+        5: "0.05",
+        10: "0.10",
+        90: "0.90",
       },
       rotate: {
-        '-22': '-22deg',
+        "-22": "-22deg",
       },
       gridTemplateRows: {
-        7: 'repeat(7, minmax(0, 1fr))',
+        7: "repeat(7, minmax(0, 1fr))",
       },
     },
   },
   variants: {},
   plugins:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? plugins
       : [...plugins, ...devOnlyPlugins],
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  experimental: {
-    uniformColorPalette: true, // color shades are similar brightness
-    extendedSpacingScale: true, // 72, 80, 96
-    extendedFontSizeScale: true, // 7xl, 8xl, 9xl
-  },
-}
+};
