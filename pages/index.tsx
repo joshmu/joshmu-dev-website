cdeafe4776f8ca8178215561ae337938a2aedf01
Josh Mu <hello@joshmu.dev>
2021-09-25 13:16:18 +1000

    :zap: fix import path

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@
  *
  * @author Josh Mu <hello@joshmu.dev>
  * @created Friday, 13th November 2020 3:44:50 pm
- * @modified Friday, 20th November 2020 4:47:47 pm
+ * @modified Saturday, 25th September 2021 1:16:07 pm
  * @copyright © 2020 - 2020 MU
  */
 
 import { Activity } from '@/components/Activity/Activity'
-import { Banner } from '@/components/Banner/Banner.tsx'
+import { Banner } from '@/components/Banner/Banner'
 import { Contact } from '@/components/Contact/Contact'
 import { Hero } from '@/components/Hero/Hero'
 import { Projects } from '@/components/Projects/Projects'
