855db0da578db0f1d76a9ed55283e6b73f40c015
Josh Mu <hello@joshmu.dev>
2021-10-09 22:18:29 +1000

    reveal custom cursor on entry within window

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -6,21 +6,23 @@
  *
  * @author Josh Mu <hello@joshmu.dev>
  * @created Saturday, 25th September 2021
- * @modified Saturday, 9th October 2021 10:11:28 pm
+ * @modified Saturday, 9th October 2021 10:16:49 pm
  * @copyright © 2020 - 2021 MU
  */
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { isDesktop } from 'react-device-detect'
 
 /**
  * @see https://github.com/nicubarbaros/custom-mouse-change/tree/master/src/components/CustomCursor
  */
 export const Cursor = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
   const mainCursor = useRef(null)
 
   useEffect(() => {
     const cursorMove = event => {
+      if (!isLoaded) setIsLoaded(true)
       const { clientX, clientY } = event
 
       const mouseX = clientX
@@ -40,7 +42,7 @@ export const Cursor = () => {
     <div
       ref={mainCursor}
       className='main-cursor'
-      style={{ display: isDesktop ? 'initial' : 'none' }}
+      style={{ display: isDesktop && isLoaded ? 'initial' : 'none' }}
     >
       <div className='main-cursor-background'></div>
     </div>
