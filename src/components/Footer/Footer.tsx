c211e2bc05e33a30f67d06c4cdedd2b4df63f7c9
Josh Mu <hello@joshmu.dev>
2022-01-26 18:19:05 +1100

    allow cursor animation for back to top icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { BackToTop } from './BackToTop/BackToTop'
 
 export const Footer = () => {
   return (
-    <div className='pb-4 text-center'>
+    <div className='relative pb-4 text-center'>
       <div className='pb-12 pt-36'>
         <p>{new Date().getFullYear()}</p>
         <p>✌️</p>
